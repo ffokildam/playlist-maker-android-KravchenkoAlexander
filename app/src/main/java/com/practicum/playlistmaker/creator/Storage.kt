a1b9ce70d795d6ae423ab5067af3d1b5c05366c1
Alexander <akravchenko@sfedu.ru>
2025-11-05 12:29:25 +0300

    finish sprint6

--- app/src/main/java/com/practicum/playlistmaker/creator/Storage.kt
+++ app/src/main/java/com/practicum/playlistmaker/creator/Storage.kt
@@ -7,17 +7,17 @@ class Storage {
         TrackDto(
             trackName = "Владивосток 2000",
             artistName = "Мумий Троль",
-            trackTimeMillis = 158000 // 2:38
+            trackTimeMillis = 158000
         ),
         TrackDto(
             trackName = "Группа крови",
             artistName = "Кино",
-            trackTimeMillis = 283000 // 4:43
+            trackTimeMillis = 283000
         ),
         TrackDto(
             trackName = "Не смотри назад",
             artistName = "Ария",
-            trackTimeMillis = 312000 // 5:12
+            trackTimeMillis = 312000
         ),
         TrackDto(
             trackName = "Звезда по имени Солнце",
@@ -57,10 +57,10 @@ class Storage {
     )
 
     fun search(request: String): List<TrackDto> {
+        val query = request.lowercase()
         val result = listTracks.filter {
-            it.trackName
-                .lowercase()
-                .contains(request.lowercase())
+            it.trackName.lowercase().contains(query) ||
+            it.artistName.lowercase().contains(query)
         }
         return result
     }
